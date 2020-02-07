813ebd4f13589c2b3942f77a86aed5be411c157d
Ignacio Tampe Palma <thenachotp@gmail.com>
2020-02-07 15:17:07 -0400

    Discord included and tested

--- lemiknow/discord_sender.py
+++ lemiknow/discord_sender.py
@@ -9,7 +9,8 @@ import traceback
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-def discord_sender(webhook_url: str):
+
+def discord_sender(webhook_url: str, message: str = None, notify_end: bool = True, include_details: bool = True):
     """
     Discord sender wrapper: execute func, send a Discord message with the end status
     (sucessfully finished or crashed) at the end. Also send a Discord message before
@@ -19,59 +20,54 @@ def discord_sender(webhook_url: str):
         The Discord webhook URL for posting messages.
         Visit https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks to
         set up your webhook and get your URL.
+    `message`: str
+        Optional message to include when notifying the function call.
+        default: None
+    `notify_end`: bool
+        Send a notification when the function finishes (not recommended for short calls).
+        default: True
+    `include_details`: bool
+        Adds extra information on notifications like hostname, start time, etc.
+        Can't be False if message is None.
+        default: true
     """
     def decorator_sender(func):
         def send_message(text: str):
             headers = {'Content-Type': 'application/json'}
             payload = json.dumps({'content': text})
-            r = requests.post(url=webhook_url, data=payload, headers=headers)
-        
+            requests.post(url=webhook_url, data=payload, headers=headers)
+
         @functools.wraps(func)
         def wrapper_sender(*args, **kwargs):
 
             start_time = datetime.datetime.now()
             host_name = socket.gethostname()
             func_name = func.__name__
+            text = ""
+            if include_details:
+                text += f'{func_name} called on {host_name} at {start_time.strftime(DATE_FORMAT)}'
+            if message:
+                text += f'{message}' if not include_details else f'\nMessage: {message}'
+            if notify_end:
+                text += '\nWe\'ll let you know when it\'s done.'
 
-            # Handling distributed training edge case.
-            # In PyTorch, the launch of `torch.distributed.launch` sets up a RANK environment variable for each process.
-            # This can be used to detect the master process.
-            # See https://github.com/pytorch/pytorch/blob/master/torch/distributed/launch.py#L211
-            # Except for errors, only the master process will send notifications.
-            if 'RANK' in os.environ:
-                master_process = (int(os.environ['RANK']) == 0)
-                host_name += ' - RANK: %s' % os.environ['RANK']
-            else:
-                master_process = True
-
-            if master_process:
-                contents = ['Your training has started 🎬',
-                            'Machine name: %s' % host_name,
-                            'Main call: %s' % func_name,
-                            'Starting date: %s' % start_time.strftime(DATE_FORMAT)]
-                text = '\n'.join(contents)
-                send_message(text=text)
+            send_message(text=text)
 
             try:
                 value = func(*args, **kwargs)
-
-                if master_process:
+                if notify_end:
                     end_time = datetime.datetime.now()
                     elapsed_time = end_time - start_time
-                    contents = ["Your training is complete 🎉",
-                                'Machine name: %s' % host_name,
-                                'Main call: %s' % func_name,
-                                'Starting date: %s' % start_time.strftime(DATE_FORMAT),
-                                'End date: %s' % end_time.strftime(DATE_FORMAT),
-                                'Training duration: %s' % str(elapsed_time)]
+                    text = ""
+                    text += f'✅ {func_name} finished on {host_name} at {end_time.strftime(DATE_FORMAT)}'
+                    text += f'\nDuration: {elapsed_time}'
 
                     try:
                         str_value = str(value)
-                        contents.append('Main call returned value: %s'% str_value)
+                        text += f'\nReturned value: {str_value}'
                     except:
-                        contents.append('Main call returned value: %s'% "ERROR - Couldn't str the returned value.")
+                        text += f'\nReturned value: ERROR - Couldn\'t parse the returned value.'
 
-                    text = '\n'.join(contents)
                     send_message(text=text)
 
                 return value
@@ -79,12 +75,7 @@ def discord_sender(webhook_url: str):
             except Exception as ex:
                 end_time = datetime.datetime.now()
                 elapsed_time = end_time - start_time
-                contents = ["Your training has crashed ☠️",
-                            'Machine name: %s' % host_name,
-                            'Main call: %s' % func_name,
-                            'Starting date: %s' % start_time.strftime(DATE_FORMAT),
-                            'Crash date: %s' % end_time.strftime(DATE_FORMAT),
-                            'Crashed training duration: %s\n\n' % str(elapsed_time),
+                contents = [f"☠️ {func_name} has crashed on {host_name} at {end_time.strftime(DATE_FORMAT)}",
                             "Here's the error:",
                             '%s\n\n' % ex,
                             "Traceback:",
