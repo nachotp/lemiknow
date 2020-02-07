9db6238a1769f3ea6c98a346843802d1c73002d8
Ignacio Tampe Palma <thenachotp@gmail.com>
2020-02-06 23:36:28 -0400

    First push from knockknock

---
+++ lemiknow/__main__.py
@@ -0,0 +1,133 @@
+import argparse
+import subprocess
+
+from knockknock import email_sender, slack_sender, telegram_sender, teams_sender, sms_sender, discord_sender, desktop_sender, matrix_sender
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="KnockKnock - Be notified when your training is complete.")
+    parser.add_argument("--verbose", required=False, action="store_true",
+                        help="Show full command in notification.")
+    subparsers = parser.add_subparsers()
+
+    ## Desktop
+    desktop_parser = subparsers.add_parser(
+        name="desktop", description="Send a desktop notification before and after function " +
+        "execution, with start and end status (successfully or crashed).")
+    desktop_parser.add_argument("--title", type=str, required=False,
+        help="The title of the notification, default to knockknock")
+    desktop_parser.set_defaults(sender_func=desktop_sender)
+    
+    ## Discord
+    discord_parser = subparsers.add_parser(
+        name="discord", description="Send a Discord message before and after function " +
+        "execution, with start and end status (sucessfully or crashed).")
+    discord_parser.add_argument(
+        "--webhook-url", type=str, required=True,
+        help="The webhook URL to access your Discord server/channel.")
+    discord_parser.set_defaults(sender_func=discord_sender)
+
+    ## Email
+    email_parser = subparsers.add_parser(
+        name="email", description="Send an email before and after function " +
+        "execution, with start and end status (sucessfully or crashed).")
+    email_parser.add_argument(
+        "--recipient-emails", type=lambda s: s.split(","), required=True,
+        help="The email addresses to notify, as comma separated list.")
+    email_parser.add_argument(
+        "--sender-email", type=str, required=False,
+        help="The email adress to send the messages." +
+        "(default: use the same address as the first email in `recipient-emails`)")
+    email_parser.set_defaults(sender_func=email_sender)
+
+    ## Slack
+    slack_parser = subparsers.add_parser(
+        name="slack", description="Send a Slack message before and after function " +
+        "execution, with start and end status (sucessfully or crashed).")
+    slack_parser.add_argument(
+        "--webhook-url", type=str, required=True,
+        help="The webhook URL to access your slack room.")
+    slack_parser.add_argument(
+        "--channel", type=str, required=True, help="The slack room to log.")
+    slack_parser.add_argument(
+        "--user-mentions", type=lambda s: s.split(","), required=False, default=[],
+        help="Optional user ids to notify, as comma seperated list.")
+    slack_parser.set_defaults(sender_func=slack_sender)
+
+    ## Telegram
+    telegram_parser = subparsers.add_parser(
+        name="telegram", description="Send a Telegram message before and after " +
+        "function execution, with start and end status (sucessfully or crashed).")
+    telegram_parser.add_argument(
+        "--token", type=str, required=True,
+        help="The API access TOKEN required to use the Telegram API.")
+    telegram_parser.add_argument(
+        "--chat-id", type=int, required=True,
+        help="Your chat room id with your notification BOT.")
+    telegram_parser.set_defaults(sender_func=telegram_sender)
+
+    ## Teams
+    teams_parser = subparsers.add_parser(
+        name="teams", description="Send a teams message before and after function " +
+        "execution, with start and end status (sucessfully or crashed).")
+    teams_parser.add_argument(
+        "--webhook-url", type=str, required=True,
+        help="The webhook URL to access your teams channel.")
+    teams_parser.add_argument(
+        "--user-mentions", type=lambda s: s.split(","), required=False, default=[],
+        help="Optional user ids to notify, as comma seperated list.")
+    teams_parser.set_defaults(sender_func=teams_sender)
+
+    ## SMS
+    sms_parser = subparsers.add_parser(
+        name="sms", description="Send an SMS using the Twilio API")
+    sms_parser.add_argument(
+        "--account-sid", type=str, required=True,
+        help="The account SID to access your Twilio account.")
+    sms_parser.add_argument(
+        "--auth-token", type=str, required=True,
+        help="The authentication token to access your Twilio account.")
+    sms_parser.add_argument(
+        "--recipient-number", type=str, required=True,
+        help="The phone number of the recipient.")
+    sms_parser.add_argument(
+        "--sender-number", type=str, required=True,
+        help="The phone number of the sender (Twilio number).")
+    sms_parser.set_defaults(sender_func=sms_sender)
+
+    ## Matrix
+    matrix_parser = subparsers.add_parser(
+        name="matrix", description="Send a Matrix message before and after " +
+        "function execution, with start and end status (sucessfully or crashed).")
+    matrix_parser.add_argument(
+        "--homeserver", type=str, required=True,
+        help="The homeserver address which was used to register the BOT.")
+    matrix_parser.add_argument(
+        "--token", type=str, required=True,
+        help="The access TOKEN of the user that will send the messages.")
+    matrix_parser.add_argument(
+        "--room", type=str, required=True,
+        help="The alias of the room to which messages will be send by the BOT.")
+    matrix_parser.set_defaults(sender_func=matrix_sender)
+
+    args, remaining_args = parser.parse_known_args()
+    args = vars(args)
+
+    sender_func = args.pop("sender_func", None)
+
+    if sender_func is None:
+        parser.print_help()
+        exit(1)
+
+    verbose = args.pop("verbose")
+
+    def run_func(): return subprocess.run(remaining_args, check=True)
+    run_func.__name__ = " ".join(
+        remaining_args) if verbose else remaining_args[0]
+
+    sender_func(**args)(run_func)()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lemiknow/email_sender.py
@@ -0,0 +1,99 @@
+import os
+import datetime
+import traceback
+import functools
+import socket
+import yagmail
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+def email_sender(recipient_emails: list, sender_email: str = None):
+    """
+    Email sender wrapper: execute func, send an email with the end status
+    (sucessfully finished or crashed) at the end. Also send an email before
+    executing func.
+
+    `recipient_emails`: list[str]
+        A list of email addresses to notify.
+    `sender_email`: str (default=None)
+        The email adress to send the messages. If None, use the same
+        address as the first recipient email in `recipient_emails`
+        if length of `recipient_emails` is more than 0.
+    """
+    if sender_email is None and len(recipient_emails) > 0:
+        sender_email = recipient_emails[0]
+    yag_sender = yagmail.SMTP(sender_email)
+
+    def decorator_sender(func):
+        @functools.wraps(func)
+        def wrapper_sender(*args, **kwargs):
+
+            start_time = datetime.datetime.now()
+            host_name = socket.gethostname()
+            func_name = func.__name__
+
+            # Handling distributed training edge case.
+            # In PyTorch, the launch of `torch.distributed.launch` sets up a RANK environment variable for each process.
+            # This can be used to detect the master process.
+            # See https://github.com/pytorch/pytorch/blob/master/torch/distributed/launch.py#L211
+            # Except for errors, only the master process will send notifications.
+            if 'RANK' in os.environ:
+                master_process = (int(os.environ['RANK']) == 0)
+                host_name += ' - RANK: %s' % os.environ['RANK']
+            else:
+                master_process = True
+
+            if master_process:
+                contents = ['Your training has started.',
+                            'Machine name: %s' % host_name,
+                            'Main call: %s' % func_name,
+                            'Starting date: %s' % start_time.strftime(DATE_FORMAT)]
+                for i in range(len(recipient_emails)):
+                    current_recipient = recipient_emails[i]
+                    yag_sender.send(current_recipient, 'Training has started 🎬', contents)
+            try:
+                value = func(*args, **kwargs)
+
+                if master_process:
+                    end_time = datetime.datetime.now()
+                    elapsed_time = end_time - start_time
+                    contents = ["Your training is complete.",
+                                'Machine name: %s' % host_name,
+                                'Main call: %s' % func_name,
+                                'Starting date: %s' % start_time.strftime(DATE_FORMAT),
+                                'End date: %s' % end_time.strftime(DATE_FORMAT),
+                                'Training duration: %s' % str(elapsed_time)]
+
+                    try:
+                        str_value = str(value)
+                        contents.append('Main call returned value: %s'% str_value)
+                    except:
+                        contents.append('Main call returned value: %s'% "ERROR - Couldn't str the returned value.")
+
+                    for i in range(len(recipient_emails)):
+                        current_recipient = recipient_emails[i]
+                        yag_sender.send(current_recipient, 'Training has sucessfully finished 🎉', contents)
+
+                return value
+
+            except Exception as ex:
+                end_time = datetime.datetime.now()
+                elapsed_time = end_time - start_time
+                contents = ["Your training has crashed.",
+                            'Machine name: %s' % host_name,
+                            'Main call: %s' % func_name,
+                            'Starting date: %s' % start_time.strftime(DATE_FORMAT),
+                            'Crash date: %s' % end_time.strftime(DATE_FORMAT),
+                            'Crashed training duration: %s\n\n' % str(elapsed_time),
+                            "Here's the error:",
+                            '%s\n\n' % ex,
+                            "Traceback:",
+                            '%s' % traceback.format_exc()]
+                for i in range(len(recipient_emails)):
+                    current_recipient = recipient_emails[i]
+                    yag_sender.send(current_recipient, 'Training has crashed ☠️', contents)
+                raise ex
+
+        return wrapper_sender
+
+    return decorator_sender
