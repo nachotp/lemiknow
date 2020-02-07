b3a49b06e4acffe612ba0a30e2e75aae0e272c51
Ignacio Tampe Palma <thenachotp@gmail.com>
2020-02-07 15:47:25 -0400

    Update to version 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from io import open
 
 setup(
     name='lemiknow',
-    version='0.0.5',
+    version='0.0.6',
     description='Let\'s you know when your function is called, ends or crashes',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
